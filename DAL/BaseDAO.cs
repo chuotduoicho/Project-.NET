f5062f47c53bb6e9875b0ee7169ea2ee155e6c92
Võ Đức Tài <vo1122000@gmail.com>
2021-08-09 22:09:36 +0700

    tai commit
    tai commit

---
+++ DAL/BaseDAO.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+
+namespace DAL
+{
+    public abstract class BaseDAO<E>
+    {
+        protected SqlConnection connection;
+        public BaseDAO()
+        {
+            connection = new SqlConnection(ConfigurationManager.ConnectionStrings["strConnection"].ToString());
+        }
+
+
+    }
+}
---
+++ Entity/Order.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Entity
+{
+    public class Order
+    {
+        public int ID { get; set; }
+        public Product Pid { get; set; }
+        public string Name { get; set; }
+        public string Tel { get; set; }
+        public string Address { get; set; }
+        public DateTime Time{ get; set; }
+        public int Status { get; set; }
+
+    }
+}
